934629d5130043953725869cd3d5081171545cec
Knot <swiss.knot@gmail.com>
2024-02-26 17:47:53 +0700

    [+] added readme

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,7 +4,7 @@ import { useAuthentication } from '@/hooks/useAuthentication'
 import { useEffect, useMemo } from 'react';
 
 export default function Login() {
-  const { onLogin, setPassword, setUsername, error } = useAuthentication()
+  const { onLogin, setPassword, setUsername, error, isLoading } = useAuthentication()
 
   const emailWarning = useMemo(() => {
     return error && (
@@ -63,12 +63,23 @@ export default function Login() {
                 />
                 {passwordWarning}
               </div>
-              <button
-                onClick={() => onLogin()}
-                className="w-full px-5 py-2.5 mb-6 text-white bg-indigo-400 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm text-center"
-              >
-                Submit
-              </button>
+              {
+                isLoading ? 
+                  <div className="flex justify-center mb-6">
+                    <div className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-black border-r-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                    role="status">
+                      <span
+                        className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+                      >Loading...</span>
+                    </div>
+                  </div>
+                  : <button
+                      onClick={() => onLogin()}
+                      className="w-full px-5 py-2.5 mb-6 text-white bg-indigo-400 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm text-center"
+                    >
+                      Submit
+                    </button>
+              }
             </div>
           </div>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -82,7 +82,7 @@ export default function Home() {
 
   const Loading = useMemo(() => {
     return (isLoading && <div className="flex justify-center"><div
-        className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-blue border-r-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]"
+        className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-black border-r-transparent align-[-0.125em] text-primary motion-reduce:animate-[spin_1.5s_linear_infinite]"
         role="status">
         <span
           className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
